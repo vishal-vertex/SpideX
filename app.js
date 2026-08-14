901e8164b60322a05564dc79c6f851bc196c037d
Vishal A <itz.me.vishal.24@gmail.com>
2026-08-14 17:59:16 +0530

    id

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ window.signIn = async () => {
       if (errorBox) errorBox.textContent = 'Date of Birth or Roll Number is incorrect or not registered.'
     } else {
       localStorage.setItem('user', JSON.stringify(data))
-      window.location.href = "dashboard.html?userId=" + UserId;
+      window.location.href = "dashboard.html?userId=" + UserId + randomNumber(1,100);
     }
   } catch (err) {
     console.error('Unexpected sign-in error:', err)
